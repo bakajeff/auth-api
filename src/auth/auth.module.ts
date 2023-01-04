ab86917a82df22d059d74c56060e49989157d3f9
bakajeff <jefftewf17@outlook.com>
2023-01-04 14:31:40 -0300

    style: format ts files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from 'src/users/users.module';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { UsersModule } from "src/users/users.module";
+import { AuthService } from "./auth.service";
 
 @Module({
   imports: [UsersModule],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'password',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'password',
-    },
-  ];
+	private readonly users = [
+		{
+			userId: 1,
+			username: "john",
+			password: "password",
+		},
+		{
+			userId: 2,
+			username: "maria",
+			password: "password",
+		},
+	];
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
-  }
+	async findOne(username: string): Promise<User | undefined> {
+		return this.users.find((user) => user.username === username);
+	}
 }
