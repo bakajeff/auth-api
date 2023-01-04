cbecafbcc09b3436e50630a14a315b9bd0f04c63
bakajeff <jefftewf17@outlook.com>
2023-01-04 15:06:53 -0300

    refactor: move passed AuthGuard to its own file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AppService } from "./app.service";
+import { LocalAuthGuard } from "./auth/local-auth.guard";
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@UseGuards(AuthGuard('local'))
+	@UseGuards(LocalAuthGuard)
 	@Post('auth/login')
 	login(@Request() req) {
 		return req.user;
