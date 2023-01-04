c096f67c0cc78c23ce1be96fa505d0eb247a3190
bakajeff <jefftewf17@outlook.com>
2023-01-04 15:21:24 -0300

    feat: generate an access token on login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,14 @@
 import { Controller, Post, Request, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { AppService } from "./app.service";
+import { AuthService } from "./auth/auth.service";
 import { LocalAuthGuard } from "./auth/local-auth.guard";
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	constructor(private readonly authService: AuthService) {}
 
 	@UseGuards(LocalAuthGuard)
 	@Post('auth/login')
 	login(@Request() req) {
-		return req.user;
+		return this.authService.login(req.user);
 	}
 }
